580756cf2052c75450fc69aafac3d1bd4d035623
Andres Atala <a.atala01@ufromail.cl>
2023-06-01 02:36:41 -0400

    final

---
+++ src/main/java/agenciaDeViajes.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class agenciaDeViajes {
+    private List<agenteDeViajes> agentes;
+    private List<cliente> clientes;
+
+    public agenciaDeViajes() {
+        this.agentes = new ArrayList<>();
+        this.clientes = new ArrayList<>();
+    }
+
+    public void agregarAgente(agenteDeViajes agente) {
+        agentes.add(agente);
+    }
+
+    public void agregarCliente(cliente cliente) {
+        clientes.add(cliente);
+    }
+
+    public int obtenerCantidadClientes() {
+        return clientes.size();
+    }
+
+    public int obtenerCantidadAgentes() {
+        return agentes.size();
+    }
+
+    public static void main(String[] args) {
+        agenteDeViajes agente1 = new agenteDeViajes("Juan", "Vaciones para familia");
+        cliente cliente1 = new cliente("María", "Ex linea ferrea sin numero", 19);
+
+        agenciaDeViajes agencia = new agenciaDeViajes();
+        agencia.agregarAgente(agente1);
+        agencia.agregarCliente(cliente1);
+        agencia.obtenerCantidadClientes();
+        agencia.obtenerCantidadAgentes();
+
+        int cantidadClientes = agencia.obtenerCantidadClientes();
+        int cantidadAgentes = agencia.obtenerCantidadAgentes();
+        System.out.println(cantidadAgentes);
+        System.out.println(cantidadClientes);
+
+    }
+}
---
+++ src/main/java/agenteDeViajes.java
@@ -0,0 +1,25 @@
+public class agenteDeViajes {
+    private String nombre;
+    private String especialidad;
+
+    public agenteDeViajes(String nombre, String especialidad) {
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+}
